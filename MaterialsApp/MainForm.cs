d156c47e7ada99055f80e737d37fcef5eb5695ab
User <User@WIN-32NOSR5DPIA>
2025-03-13 10:21:57 +0500

    Добавьте файлы проекта.

---
+++ MaterialsApp/MainForm.cs
@@ -0,0 +1,10 @@
+namespace MaterialsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MaterialsApp/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaterialsApp.Models;
+
+public partial class Product {
+	public short Id { get; set; }
+
+	public short IdProductType { get; set; }
+
+	public decimal PriceMin { get; set; }
+
+	public string NameOfProduct { get; set; } = null!;
+
+	public string Article { get; set; } = null!;
+
+	public virtual ProductType IdProductTypeNavigation { get; set; } = null!;
+
+	public virtual ICollection<PartnersProduct> PartnersProducts { get; set; } = new List<PartnersProduct>();
+}
